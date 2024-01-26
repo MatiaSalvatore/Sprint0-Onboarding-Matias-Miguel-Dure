864cf968b3bbe7ac5c7ba1605e54de96e0a94e95
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-26 01:03:55 -0300

    Start

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Matías";
+        String lastName = "Dure";
+        int age = 26;
+        System.out.println("Hello world!");
+        System.out.println("Hi my name is "+firstName+" "+lastName+". And I'm "+age+" years old.");
+    }
+}
