2a1a873f88ba5b271bf42dd156506c86561ce84b
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-26 08:33:28 -0300

    Day_3 finished

--- src/day_3/Main.java
+++ src/day_3/Main.java
@@ -2,10 +2,14 @@ package day_3;
 
 public class Main {
     public static void main(String[] args) {
-        String[] pets_1 = {"Loly","Felipe"};
-        String[] pets_2 = {};
-        Person person_1 = new Person("Matias","Dure",1.69,26,true,pets_1);
-        Person person_2 = new Person("Max","Power",1.8,52,false,pets_2);
+        Pet pet_1 = new Pet("Loly",10,PetType.perro);
+        Pet pet_2 = new Pet("Felipe",7,PetType.gato);
+
+        Person person_1 = new Person("Matias","Dure",1.69,26,true);
+        person_1.addPet(pet_2);
+        person_1.addPet(pet_1);
+
+        Person person_2 = new Person("Max","Power",1.8,52,false);
         person_1.presentation();
         person_2.presentation();
     }
--- src/day_3/Pet.java
+++ src/day_3/Pet.java
@@ -1,4 +1,57 @@
 package day_3;
 
 public class Pet {
+    String name;
+    int age;
+
+    private PetType petType;
+    private Person owner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Pet(String name, int age, PetType petType) {
+        this.name = name;
+        this.age = age;
+        this.petType = petType;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", petType=" + petType +
+                ", owner=" + owner +
+                '}';
+    }
 }
